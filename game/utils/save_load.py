acede2a256e45f89ac30795e09fb3d6c022bcead
reverent-girl <45223025+reverentOne@users.noreply.github.com>
2024-03-14 02:07:44 -0600

    Update save_load.py

--- game/utils/save_load.py
+++ game/utils/save_load.py
@@ -1,13 +1,14 @@
-with open('saved_games/game_state.json', 'w') as f:
-    json.dump(game_state, f)
-
-with open('saved_games/game_state.json', 'r') as f:
-    game_state = json.load(f)
-
+import json
 import os
 
-if os.path.exists('saved_games/game_state.json'):
-    with open('saved_games/game_state.json', 'r') as f:
-        game_state = json.load(f)
-else:
-    game_state = create_initial_game_state()  # You would need to define this function
+def save_game(game_state):
+    with open('../assets/data/game_state.json', 'w') as f:
+        json.dump(game_state, f)
+
+def load_game():
+    if os.path.exists('../assets/data/game_state.json'):
+        with open('../assets/data/game_state.json', 'r') as f:
+            game_state = json.load(f)
+    else:
+        game_state = create_initial_game_state()  # You would need to define this function
+    return game_state
