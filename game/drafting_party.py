71a077305c30c4de0f6f8d1051f6d6f88c8f7b67
RankleThePrankle <aidanlunt12@gmail.com>
2024-03-21 01:56:03 -0600

    added single adventurer draft

--- game/drafting_party.py
+++ game/drafting_party.py
@@ -4,7 +4,33 @@ import random
 from objects.party import party
 import numpy
 from adventurer_generation_2 import character
+# draft a single character
+def draft_character():
+    print("Pick your adventurer! You will get a choice from 3 random adventurers. Choose wisely and good luck!\n")
+    adventurers_draft_list = []
+    for _ in range(3):  
+        print("Adventurer " + str(_+1), "\n")
+        new_adventurer = character()
+        print(new_adventurer, "\n")
+        adventurers_draft_list.append(new_adventurer)
+    while True:
+        choice = input("Choose Your Adventurer [type 1, 2 or 3] ")
+        if choice in ['1', '2', '3']:
+            choice = int(choice)
+            break
+        else:
+            print("That is not an option.\n")
+    if choice == 1:
+        character = adventurers_draft_list[0]
+    elif choice == 2:
+        character = adventurers_draft_list[1]
+    elif choice == 3:
+        character = adventurers_draft_list[2]
+    print("You have chosen your adventurer.", "\n\n")
+    print(character)
+    return character
 
+#draft a party of 4 characters
 def draft_party():
     adventurers_list = []
     print("Welcome to your Adventurer Draft! Draft a party of 4 adventurers. You will get a choice from 3 random adventurers in each of 4 rounds. Choose wisely and good luck!\n")
@@ -45,6 +71,4 @@ def draft_party():
         print(adventurer)
     name = input("\nName your party:")
     party_array = numpy.array(adventurers_list) #convert list to array so it can be used in a matrix with your other parties
-
-
-    return party(name, adventurers_list, party_array, None, 1, 0)
+    return party(name, adventurers_list, None, 1, 0), party_array
